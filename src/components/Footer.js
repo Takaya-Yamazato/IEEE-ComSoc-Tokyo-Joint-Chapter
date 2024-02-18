0f6ae4e7ac6a2128d1ae585167d337aeb9fcd9f2
Takaya YAMAZATO <Takaya-Yamazato@users.noreply.github.com>
2024-02-18 20:05:33 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
 import logo from "../img/logo.svg";
-// import facebook from "../img/social/facebook.svg";
-// import instagram from '../img/social/instagram.svg'
-// import twitter from "../img/social/twitter.svg";
-// import vimeo from '../img/social/vimeo.svg'
 import { FaWifi } from "@react-icons/all-files/fa/FaWifi";
 
 const Footer = class extends React.Component {
